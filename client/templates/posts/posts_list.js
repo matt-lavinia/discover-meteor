d47ba7042420a51fcf081f391a022f984d1c9027
matt-lavinia <matt.lavinia@gmail.com>
2015-02-09 21:02:37 -0500

    initial commit

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,14 @@
+var postsData= [
+	{
+		title: 'Introducing Telescope',
+		url: 'http://www.google.com/hunkey-dory'
+	},
+	{
+		title: 'Meteor',
+		url: 'http://www.metero.com'
+	}
+];
+
+Template.postsList.helpers({
+	posts: postsData
+});
